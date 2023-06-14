a0d636d801be6ad6ea2df220ab52a78538077389
Legojavadev <legoboytucroanch@gmail.com>
2023-06-14 00:54:49 -0500

    Creación de la estructura del proyecto y la clase model

---
+++ src/test/java/com/curso/springclass/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
